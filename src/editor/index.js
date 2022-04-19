2a43edb4131b6f03ede049baa5c357566f22828a
StormVanDerPol <psostorm@gmail.com>
2022-04-19 16:14:13 +0200

    auto link

--- src/editor/index.js
+++ src/editor/index.js
@@ -1,11 +1,14 @@
 import LexicalComposer from "@lexical/react/LexicalComposer";
 import RichTextPlugin from "@lexical/react/LexicalRichTextPlugin";
 import ContentEditable from "@lexical/react/LexicalContentEditable";
-import { LinkNode } from "@lexical/link";
+import { LinkNode, AutoLinkNode } from "@lexical/link";
 import LinkPlugin from "@lexical/react/LexicalLinkPlugin";
+
+import theme from "./theme";
+
 import TreeViewPlugin from "./plugins/treeView";
 import ToolbarPlugin from "./plugins/toolbar";
-import theme from "./theme";
+import AutoLinkPlugin from "./plugins/autoLink";
 
 const editorConfig = {
   theme,
@@ -13,16 +16,19 @@ const editorConfig = {
     console.error("[lexical error]", e);
     // Not throwing here allows lexical to try to handle the error gracefully without losing user data.
   },
-  nodes: [LinkNode],
+  nodes: [LinkNode, AutoLinkNode],
 };
 
 export default function Editor() {
   return (
     <LexicalComposer initialConfig={editorConfig}>
-      <div className="relative bg-gray-100 rounded-md border border-transparent  focus-within:bg-blue-50 focus-within:border-blue-700 transition-colors">
+      <div className="relative bg-gray-100 rounded-md border border-transparent focus-within:bg-blue-50 focus-within:border-blue-700 transition-colors">
         <div className="p-2 mb-12">
-          <RichTextPlugin contentEditable={<ContentEditable />} />
+          <RichTextPlugin
+            contentEditable={<ContentEditable className="outline-none" />}
+          />
           <LinkPlugin />
+          <AutoLinkPlugin />
         </div>
         <ToolbarPlugin />
       </div>
