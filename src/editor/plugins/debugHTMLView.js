c64c9d4c8f5e2a29f10ee714b59f82efe99859b7
StormVanDerPol <psostorm@gmail.com>
2022-04-25 17:03:28 +0200

    basic deserializer

--- src/editor/plugins/debugHTMLView.js
+++ src/editor/plugins/debugHTMLView.js
@@ -1,6 +1,7 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import lexicalToHTML from "../utils/htmlSerializer";
+import HTMLToLexical from "../utils/lexicalSerializer";
 
 export default function DebugHTMLView() {
   const [editor] = useLexicalComposerContext();
@@ -15,18 +16,35 @@ export default function DebugHTMLView() {
     return unregister;
   }, [editor]);
 
+  const editorStateStringFromHTML = useMemo(() => JSON.stringify(HTMLToLexical(HTML), null, 4), [HTML]);
+
+  const [isValid, message] = useMemo(() => {
+    try {
+      editor.parseEditorState(editorStateStringFromHTML);
+
+      return [true, "VALID"];
+    } catch (error) {
+      console.error(error);
+      return [false, `INVALID: ${error.message}`];
+    }
+  }, [editorStateStringFromHTML]);
   return (
-    <div className="my-5 p-2 rounded bg-gray-200 shadow-md border border-gray-400">
-      <div className="text-gray-600 text-sm mb-5">rendered HTML:</div>
+    <>
+      <div className="my-5 p-2 rounded bg-gray-200 shadow-md border border-gray-400">
+        <div className="text-gray-600 text-sm mb-5">rendered HTML:</div>
 
-      <style></style>
+        <style></style>
 
-      <div
-        className="rendered-rich-text"
-        dangerouslySetInnerHTML={{
-          __html: HTML,
-        }}
-      />
-    </div>
+        <div
+          className="rendered-rich-text"
+          dangerouslySetInnerHTML={{
+            __html: HTML,
+          }}
+        />
+      </div>
+
+      <div className={`${isValid ? "bg-green-500" : "bg-red-500"} p-2 text-white text-xs`}>{message}</div>
+      <pre className="rounded p-2 bg-black text-white text-xs">{editorStateStringFromHTML}</pre>
+    </>
   );
 }
--- src/editor/utils/lexicalSerializer.js
+++ src/editor/utils/lexicalSerializer.js
@@ -1,8 +1,65 @@
 export default function HTMLToLexical(htmlString) {
   const doc = new DOMParser().parseFromString(htmlString || "", "text/html");
 
-  function domToLexical(element) {
-    const isTextNode = el.nodeType === 3;
-    const isElementNode = el.nodeType === 1;
+  const nodeMap = [];
+  let keyCounter = 0;
+
+  function domToLexical(element, key, parent) {
+    const isTextNode = element.nodeType === 3;
+    const isElementNode = element.nodeType === 1;
+
+    if (isElementNode) {
+      const childKeys = Array.from(element.childNodes).map((childNode) => domToLexical(childNode, keyCounter++, key));
+
+      switch (element.nodeName) {
+        case "BODY": {
+          const rootNode = {};
+          rootNode.__children = childKeys;
+          rootNode.__dir = "ltr";
+          rootNode.__format = 0;
+          rootNode.__indent = 0;
+          rootNode.__key = key;
+          rootNode.__parent = null;
+          rootNode.__type = "root";
+
+          nodeMap.push([key, rootNode]);
+          break;
+        }
+
+        default: {
+          const node = {};
+
+          node.__children = childKeys;
+          node.__dir = "ltr";
+          node.__format = 0;
+          node.__indent = 0;
+          node.__key = key;
+          node.__parent = parent;
+          node.__type = "paragraph";
+
+          nodeMap.push([key, node]);
+        }
+      }
+    } else if (isTextNode) {
+      const textNode = {};
+
+      textNode.__detail = 0;
+      // todo: parse format
+      textNode.__format = 0;
+      textNode.__key = key;
+      textNode.__mode = 0;
+      textNode.__parent = parent;
+      textNode.__style = "";
+      textNode.__text = element.textContent;
+      textNode.__type = "text";
+
+      nodeMap.push([key, textNode]);
+    }
+
+    return key;
   }
+
+  domToLexical(doc.body, "root", null);
+
+  return { _nodeMap: nodeMap, _selection: null };
 }
